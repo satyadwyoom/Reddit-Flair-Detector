1092f0ce7b1daec5995e4ea830288de9df08a249
SATYADWYOOM <46390390+satya29m3@users.noreply.github.com>
2020-04-09 18:10:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify, render_template
+import utils
+import os
+from werkzeug.utils import secure_filename
+
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home() :
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    url = request.form['URL']
+    prediction_text = 'predicted flare : ' + utils.model_prediction(url)
+    return render_template('index.html',prediction_text= prediction_text)
+
+@app.route('/automated_testing',methods=['POST'])
+def automated_testing():
+    r = request.files['file']
+    filename = secure_filename(r.filename)
+
+    r.save(os.path.join('uploaded_data',filename))
+
+    with open('uploaded_data/'+filename,'r') as f:
+        data = f.read()
+        ret_val = dict()
+        for i in data.split():
+            prediction = utils.model_prediction(i)
+            ret_val[i] = prediction
+        return jsonify(ret_val)
+
+    return "donononnonn"
+
+
+if(__name__)=='__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,47 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+import praw
+import pickle
+import numpy as np
+
+def preprocess(x):
+    tokenizer = TweetTokenizer()
+    tokens = tokenizer.tokenize(x)
+    toks = [i for i in tokens if i not in list(stopwords.words('english'))]
+    return ' '.join(toks)
+
+def model_prediction(url):
+    model = pickle.load(open('ML_models/svc_model.pkl','rb'))
+
+    # #### ENTER YOUR CREDENTIALS #### #
+
+    client_id = 'dip10KQQt-Xg3w'
+    secret = '6R5k7o8Yf3NTfdTI3-TVPMXLIeE'
+    username = 'midas_task'
+    password = '123456789'
+
+    ### NOT NECESSARY TO CHANGE ####
+    user_agent = 'flair_task'
+
+    reddit = praw.Reddit(client_id=client_id,client_secret = secret, username=username,password=password,user_agent=user_agent)
+    submission = reddit.submission(url=url)
+    text = submission.title
+    text = preprocess(text)
+    prediction = model.predict([text])
+    prediction_text = id_2_lab(prediction)
+    return prediction_text
+
+def id_2_lab(predicted):
+    y_id = predicted[0]
+    id_2_label = {0:'Politics',
+    1:'Coronavirus',
+    2:'AskIndia',
+    3:'Non-Political',
+    4:'Policy/Economy',
+    5:'Scheduled',
+    6:'Business/Finance',
+    7:'Science/Technology',
+    8:'Food',
+    9:'Photography'}
+
+    return id_2_label[y_id]
